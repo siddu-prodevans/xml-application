e3afd2cba7fcc6af2de9d26cad51390cfcc7a04a
Siddappa Sandur <siddappa@prodevans.com>
2018-11-17 10:51:37 +0530

    Update DemoxmlApplication.java

--- src/main/java/com/demo/demoxml/DemoxmlApplication.java
+++ src/main/java/com/demo/demoxml/DemoxmlApplication.java
@@ -3,6 +3,7 @@ package com.demo.demoxml;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class DemoxmlApplication {
 
